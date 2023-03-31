24dce7a87dbddb6adbcaad1100a0457ad4952b92
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-04-01 01:39:36 +0530

    Find Max String

--- Program.cs
+++ Program.cs
@@ -7,16 +7,16 @@
             Console.WriteLine("Welcome to the Test Maximum with Generics Problem");
 
             Console.WriteLine("Please enter the first number");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            string name1 = Console.ReadLine();
 
             Console.WriteLine("Please enter the second number");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+            string name2 = Console.ReadLine();
 
             Console.WriteLine("Please enter the third number");
-            double num3 = Convert.ToDouble(Console.ReadLine());
+            string name3 = Console.ReadLine();
 
-            double val = FindMaxFloat_UC2.MaximumFloatNumber(num1, num2, num3);
-            Console.WriteLine("{0} IS GREATER  NUMBER.", val);
+            string val = FindMaxString_UC3.MaximumFloatNumber(name1, name2, name3);
+            Console.WriteLine("{0} IS GREATER  VALUE.", val);
         }
     }
 }
