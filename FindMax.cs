3a9e10e27e8460f5a662a570fa96f63ce9096782
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-04-01 01:45:53 +0530

    Max Extend 1

---
+++ FindMax.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximumValue
+{
+    public class FindMax<T> where T : IComparable
+    {
+        private T[] values;
+
+        public FindMax(params T[] values)
+        {
+            this.values = values;
+        }
+
+        public T MaximumNum()
+        {
+            T max = values[0];
+            for (int i = 1; i < values.Length; i++)
+            {
+                if (values[i].CompareTo(max) > 0)
+                {
+                    max = values[i];
+                }
+            }
+            return max;
+        }
+
+        public T TestMaximum()
+        {
+            T max = MaximumNum();
+            return max;
+        }
+    }
+}
