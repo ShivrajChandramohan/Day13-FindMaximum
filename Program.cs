1795b2d3ebd6b1ed7352b715a066c1e00b6e9ba2
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-04-01 01:14:08 +0530

    Find Max

--- Program.cs
+++ Program.cs
@@ -4,9 +4,19 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Test Maximum with Generics Problem");
+            Console.WriteLine("Welcome to the Find Maximum values with Generics Problem");
 
+            Console.WriteLine("Please enter the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Please enter the second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the third number");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int val = FindMaxInteger_UC1.MaximumIntegerNumber(num1, num2, num3);
+            Console.WriteLine("{0} IS GREATER  NUMBER.", val);
         }
     }
 }
