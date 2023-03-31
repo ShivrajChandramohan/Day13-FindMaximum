e05514e94aae14026596366a588fd1a2e7ef3566
Shivrajchandramohan <shivrajchandramohan@gmail.com>
2023-04-01 01:35:10 +0530

    Find Max Float

--- Program.cs
+++ Program.cs
@@ -4,18 +4,18 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Find Maximum values with Generics Problem");
+            Console.WriteLine("Welcome to the Test Maximum with Generics Problem");
 
             Console.WriteLine("Please enter the first number");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            double num1 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Please enter the second number");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            double num2 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Please enter the third number");
-            int num3 = Convert.ToInt32(Console.ReadLine());
+            double num3 = Convert.ToDouble(Console.ReadLine());
 
-            int val = FindMaxInteger_UC1.MaximumIntegerNumber(num1, num2, num3);
+            double val = FindMaxFloat_UC2.MaximumFloatNumber(num1, num2, num3);
             Console.WriteLine("{0} IS GREATER  NUMBER.", val);
         }
     }
